a7f0580f5e7681d874d2a413a68cc8124e62224b
Milan Lamsal <milan123lamsal@gmail.com>
2025-04-18 11:49:40 -0400

    Worked at BestSeller, OurPolicy, Component

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+    return (
+        <div className='text-center'>
+            <p className='text-2xl font-medium text-gray-800'>
+                Subscribe now & get 20% off
+            </p>
+            <p className='text-gray-400 mt-3'>
+                Get exclusive offers, updates & more straight to your inbox.
+            </p>
+
+        </div>
+    )
+}
+
+export default NewsLetterBox
